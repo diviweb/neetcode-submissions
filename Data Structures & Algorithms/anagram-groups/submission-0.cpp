a127969ec7a159bf59a1608422f91caf43c829a2
diviweb <73582624+diviweb@users.noreply.github.com>
2026-04-22 21:17:17 +0530

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> ans;
+        unordered_map<string, vector<string>> mp;
+        for(int i = 0; i < strs.size(); i++) {
+            string temp = strs[i];
+            sort(strs[i].begin(), strs[i].end());
+            mp[strs[i]].push_back(temp);
+        }
+        for(auto i = mp.begin(); i != mp.end(); i++) {
+            ans.push_back(i->second);
+        }
+        return ans;
+    }
+};
