d33ed6421aba214211723dcee24c69e3237959c5
diviweb <73582624+diviweb@users.noreply.github.com>
2026-05-28 10:43:59 +0530

    Add: valid-binary-search-tree - submission-0

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-0.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    vector<int> post;
+
+    void preOrder(TreeNode* root) {
+        if(!root)
+            return;
+        preOrder(root -> left);
+        post.push_back(root->val);
+        preOrder(root -> right);
+    }
+
+    bool isValidBST(TreeNode* root) {
+        preOrder(root);
+        for(int i = 0; i < post.size()-1; i++) {
+            if(post[i] >= post[i+1])
+                return false;
+        }
+        return true;
+    }
+};
