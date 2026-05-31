0cf83b1e9be66e86786f8aee68308c0ea0bd768e
diviweb <73582624+diviweb@users.noreply.github.com>
2026-05-31 16:37:39 +0530

    Add: kth-largest-integer-in-a-stream - submission-0

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-0.cpp
@@ -0,0 +1,21 @@
+class KthLargest {
+public:
+    priority_queue<int, vector<int>, greater<int>> minh;
+    int k;
+
+    KthLargest(int k, vector<int>& nums) {
+        this -> k = k;
+        for(int i = 0; i < nums.size(); i++) {
+            minh.push(nums[i]);
+            if(minh.size() > k)
+                minh.pop();
+        }
+    }
+    
+    int add(int val) {
+        minh.push(val);
+        if(minh.size() > k)
+            minh.pop();
+        return minh.top();
+    }
+};
