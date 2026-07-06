e48d342cdaf436f81a3ca54b3515d53498a0c7a2
diviweb <73582624+diviweb@users.noreply.github.com>
2026-07-07 02:18:06 +0530

    Add: remove-element - submission-0

---
+++ Data Structures & Algorithms/remove-element/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int k = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            int j = nums.size()-1;
+            if(nums[i] == val) {
+                nums[i] = 51;
+                k++;
+            }
+        }
+        sort(nums.begin(), nums.end());
+        return nums.size()-k;
+    }
+};
