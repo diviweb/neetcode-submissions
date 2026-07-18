411eb5631c9722ec0b3a0c0413ba34e2d37978e5
diviweb <73582624+diviweb@users.noreply.github.com>
2026-07-19 02:28:38 +0530

    Add: length-of-last-word - submission-0

---
+++ Data Structures & Algorithms/length-of-last-word/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int j = 0;
+        for(int i = s.size()-1; i >= 0; i--) {
+            if(s[i] != ' ')
+                j++;
+            else if(j != 0)
+                return j;
+        }
+        return j;
+    }
+};
